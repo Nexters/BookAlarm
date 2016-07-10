d16b9abc184f0f6f28ff627dc902fa36a97ebaf8
youngdo <youngdo9647@gmail.com>
2016-07-10 15:48:44 +0900

    new Package name

---
+++ app/src/main/java/com/nexters/paperfume/MainActivity.java
@@ -0,0 +1,13 @@
+package com.nexters.paperfume;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
