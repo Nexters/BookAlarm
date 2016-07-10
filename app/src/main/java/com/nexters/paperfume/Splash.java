0f4440b3be734562351f273f8c4d991fdcf3b9a5
youngdo <youngdo9647@gmail.com>
2016-07-10 15:54:11 +0900

    splash

---
+++ app/src/main/java/com/nexters/paperfume/Splash.java
@@ -0,0 +1,24 @@
+package com.nexters.paperfume;
+
+import android.os.Bundle;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Youngdo on 2016-07-10.
+ */
+public class Splash extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        android.os.Handler handler = new android.os.Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                finish();
+            }
+        };
+        handler.sendEmptyMessageDelayed(0, 3000);
+    }
+}
