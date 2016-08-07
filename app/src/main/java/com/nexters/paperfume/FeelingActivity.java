e148024812fba0d880464155f193f9257b20fce9
sangyeonK <sovietist@hotmail.com>
2016-08-07 19:01:37 +0900

    written detail the feeling expression

--- app/src/main/java/com/nexters/paperfume/FeelingActivity.java
+++ app/src/main/java/com/nexters/paperfume/FeelingActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+
+import java.util.Random;
 
 /**
  * Created by user on 2016-07-27.
@@ -21,6 +24,41 @@ public class FeelingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_feeling);
         button = (Button) findViewById(R.id.feeling_button);
         radioGroup = (RadioGroup) findViewById(R.id.feeling_group);
+
+        //라디오버튼에 랜덤 텍스트 입력
+        {
+            Random r = new Random();
+            RadioButton radioButton = null;
+            String[] sentences = null;
+            int index;
+
+            for(int i = 0 ; i < 4 ; i++)
+            {
+                switch(i) {
+                    case 0:
+                        radioButton = (RadioButton) findViewById(R.id.feeling_happy);
+                        sentences = getResources().getStringArray(R.array.feeling_happy);
+                        break;
+                    case 1:
+                        radioButton = (RadioButton) findViewById(R.id.feeling_miss);
+                        sentences = getResources().getStringArray(R.array.feeling_miss);
+                        break;
+                    case 2:
+                        radioButton = (RadioButton) findViewById(R.id.feeling_groomy);
+                        sentences = getResources().getStringArray(R.array.feeling_groomy);
+                        break;
+                    case 3:
+                        radioButton = (RadioButton) findViewById(R.id.feeling_stifled);
+                        sentences = getResources().getStringArray(R.array.feeling_stifled);
+                        break;
+                }
+
+                if(radioButton != null) {
+                    index = r.nextInt(sentences.length);
+                    radioButton.setText(sentences[index]);
+                }
+            }
+        }
 
         button.setOnClickListener(new Button.OnClickListener(){
             @Override
