1be5700ea23126a08530aabc311e342fbda059d4
chaeyoungdo <dudeh534@naver.com>
2016-08-13 17:55:22 +0900

    Firebase realtime database Book List getting!

--- app/src/main/java/com/nexters/paperfume/PerfumeActivity.java
+++ app/src/main/java/com/nexters/paperfume/PerfumeActivity.java
@@ -1,11 +1,19 @@
 package com.nexters.paperfume;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
 
 /**
  * Created by user on 2016-07-27.
@@ -13,18 +21,32 @@ import android.widget.Button;
 
 public class PerfumeActivity extends AppCompatActivity {
     Button button;
-
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("recommend_books");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_perfume);
         button = (Button) findViewById(R.id.getting_books);
+        final Intent intent = getIntent();
         button.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view) {
                 //Selected feelings post to server
 
+                myRef.child("by_feeling").child(intent.getStringExtra("feeling")).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        List object = (List) dataSnapshot.getValue();
+
+                        Log.e("value",object.get(1).toString());
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
                 Intent intent = new Intent(PerfumeActivity.this, MainActivity.class);
                 //intent.putExtra() //books data
                 startActivity(intent);
@@ -32,4 +54,5 @@ public class PerfumeActivity extends AppCompatActivity {
             }
         });
     }
+
 }
