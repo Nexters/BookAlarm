08214a5b7002414fe7aab91a3a17e1ab0f4aab9f
chaeyoungdo <dudeh534@naver.com>
2016-08-13 21:23:47 +0900

    firebase storage file getting!

--- app/src/main/java/com/nexters/paperfume/PerfumeActivity.java
+++ app/src/main/java/com/nexters/paperfume/PerfumeActivity.java
@@ -18,8 +18,12 @@ import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 /**
@@ -62,11 +66,51 @@ public class PerfumeActivity extends AppCompatActivity {
 
 
                 try {
-                    File localFile = File.createTempFile("19418350","json");
+                    final File localFile = File.createTempFile("19418350","json");
                     islandRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
                         @Override
                         public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
-                            Log.e("?", "?");
+                            try {
+                                FileInputStream fs = new FileInputStream(localFile);
+                                BufferedReader bf = new BufferedReader(new InputStreamReader(fs));
+                                StringBuilder sb = new StringBuilder();
+                                int resultSize = 0;
+                                int buffersize = Character.MAX_VALUE;
+                                char readBuf[] = new char[buffersize];
+                                while((resultSize = bf.read(readBuf)) != -1){
+                                    if(resultSize == buffersize){
+                                        sb.append(readBuf);
+                                    }else{
+                                        for(int i = 0;i<resultSize;i++){
+                                            sb.append(readBuf[i]);
+                                        }
+                                    }
+                                }
+                                String jString = sb.toString();
+                                Log.e("test",jString);
+                            } catch (FileNotFoundException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            try {
+                                Log.e("jsonparser",localFile.getCanonicalPath());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            /*
+                            try {
+                                //Object object = parser.parse(new FileReader(localFile));
+                                //JSONArray jsonObject = (JSONArray) object;
+
+
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }*/
                         }
                     }).addOnFailureListener(new OnFailureListener() {
                         @Override
