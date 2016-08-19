49e98481d9c2b6641624b01ad406331327a2fd73
sangyeonK <sovietist@hotmail.com>
2016-08-19 11:49:43 +0900

    loading logo image fade in

--- app/src/main/java/com/nexters/paperfume/Splash.java
+++ app/src/main/java/com/nexters/paperfume/Splash.java
@@ -22,6 +22,7 @@ import android.animation.ValueAnimator;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -73,90 +74,14 @@ public class Splash extends AppCompatActivity {
         MyBook.getInstance().init(getApplicationContext());
 
         setContentView(R.layout.activity_splash);
-/*
-        final LinearLayout backgroundOne = (LinearLayout) findViewById(R.id.splash);
-        final LinearLayout backgroundTwo = (LinearLayout) findViewById(R.id.splash2);
-        final RelativeLayout foreground = (RelativeLayout) findViewById(R.id.splash3);
-        final TextView text1 = (TextView) findViewById(R.id.splashText);
-        final TextView text2 = (TextView) findViewById(R.id.splashText2);
 
-        final ValueAnimator animator = ValueAnimator.ofFloat(0.0f, 1.0f);
-        animator.setRepeatCount(ValueAnimator.INFINITE);
-        animator.setInterpolator(new LinearInterpolator());
-        animator.setDuration(80000L);
+        final ImageView loadingLogo = (ImageView) findViewById(R.id.loading_logo);
+        ObjectAnimator fadeIn = ObjectAnimator.ofFloat(loadingLogo,"alpha",0.0f, 1.0f);
+        fadeIn.setStartDelay(1000L);
+        fadeIn.setDuration(2000L);
 
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                final float progress = (float) animation.getAnimatedValue();
-                final float width = backgroundOne.getWidth();
-                final float translationX = -(width * progress);
-                backgroundOne.setTranslationX(translationX);
-                backgroundTwo.setTranslationX(translationX + width);
-            }
-        });
+        fadeIn.start();
 
-        animator.start();
-
-        ObjectAnimator fadeIn = ObjectAnimator.ofFloat(foreground, "alpha", .1f, 1f);
-        fadeIn.setDuration(1000);
-        ObjectAnimator fadeIn2 = ObjectAnimator.ofFloat(text1, "alpha", .1f, 1f);
-        fadeIn2.setDuration(700);
-        ObjectAnimator delay = ObjectAnimator.ofFloat(text1, "alpha", 1f, 1f);
-        delay.setDuration(1000);
-        ObjectAnimator fadeIn3 = ObjectAnimator.ofFloat(text2, "alpha", .1f, 1f);
-        fadeIn3.setDuration(700);
-
-        final TranslateAnimation slide = new TranslateAnimation(0, 0, 0, -200);
-        slide.setDuration(500);
-        slide.setFillAfter(true);
-
-        text1.setVisibility(View.INVISIBLE);
-        text2.setVisibility(View.INVISIBLE);
-
-        final AnimatorSet mAnimationSet = new AnimatorSet();
-        final AnimatorSet mAnimationSet2 = new AnimatorSet();
-        final AnimatorSet mDelay = new AnimatorSet();
-        final AnimatorSet mAnimationSet3 = new AnimatorSet();
-
-        mAnimationSet.play(fadeIn);
-        mAnimationSet2.play(fadeIn2);
-        mDelay.play(delay);
-        mAnimationSet3.play(fadeIn3);
-
-        mAnimationSet.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                text1.setVisibility(View.VISIBLE);
-                mAnimationSet2.start();
-            }
-        });
-
-        mAnimationSet2.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                mDelay.start();
-            }
-        });
-
-        mDelay.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-                super.onAnimationEnd(animation);
-                text1.startAnimation(slide);
-            }
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                super.onAnimationEnd(animation);
-                text2.setVisibility(View.VISIBLE);
-                mAnimationSet3.start();
-            }
-        });
-
-        mAnimationSet.start();
-*/
         mLoginFailedDialog = new AlertDialog.Builder(Splash.this).create();
         mLoginFailedDialog.setTitle(R.string.error);
         mLoginFailedDialog.setMessage(getResources().getString(R.string.failed_login));
