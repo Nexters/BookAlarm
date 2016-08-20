936a155e63837ab111429039109d38646c6eba93
sangyeonK <sovietist@hotmail.com>
2016-08-20 12:57:56 +0900

    modify feeling activity

--- app/src/main/java/com/nexters/paperfume/FeelingActivity.java
+++ app/src/main/java/com/nexters/paperfume/FeelingActivity.java
@@ -1,23 +1,20 @@
 package com.nexters.paperfume;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
-import com.google.gson.Gson;
 import com.nexters.paperfume.content.Status;
 import com.nexters.paperfume.enums.Feeling;
 
 import com.nexters.paperfume.util.CustomFont;
 import com.nexters.paperfume.util.BackPressCloseHandler;
 
-import java.util.Random;
+import java.util.ArrayList;
 
 
 /**
@@ -27,6 +24,7 @@ import java.util.Random;
 public class FeelingActivity extends AppCompatActivity {
     Button button;
     RadioGroup radioGroup;
+    ArrayList<RadioButton> mRadioButtons = new ArrayList<RadioButton>();
     private Feeling feeling;
     private BackPressCloseHandler backPressCloseHandler;
 
@@ -38,44 +36,76 @@ public class FeelingActivity extends AppCompatActivity {
         button.setVisibility(View.INVISIBLE);
         radioGroup = (RadioGroup) findViewById(R.id.feeling_group);
         radioGroup.setOnCheckedChangeListener(radioGroup_Listner);
-
         backPressCloseHandler = new BackPressCloseHandler(this);
 
-        //폰트 설정
-        //라디오버튼에 랜덤 텍스트 입력
+        //폰트 설정, 버튼들 하드코딩...(RadioGroup 이 child layout 처리를 못해줌 )
         {
-            Random r = new Random();
             RadioButton radioButton = null;
-            String[] sentences = null;
-            int index;
 
             for(int i = 0 ; i < 4 ; i++)
             {
                 switch(i) {
                     case 0:
                         radioButton = (RadioButton) findViewById(R.id.feeling_happy);
-                        sentences = getResources().getStringArray(R.array.feeling_happy);
+                        radioButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                for(RadioButton bt : mRadioButtons) {
+                                    bt.setChecked(false);
+                                }
+                                feeling = Feeling.HAPPY;
+                                ((RadioButton)view).setChecked(true);
+                                button.setVisibility(View.VISIBLE);
+                            }
+                        });
                         break;
                     case 1:
                         radioButton = (RadioButton) findViewById(R.id.feeling_miss);
-                        sentences = getResources().getStringArray(R.array.feeling_miss);
+                        radioButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                for(RadioButton bt : mRadioButtons) {
+                                    bt.setChecked(false);
+                                }
+                                feeling = Feeling.MISS;
+                                ((RadioButton)view).setChecked(true);
+                                button.setVisibility(View.VISIBLE);
+                            }
+                        });
                         break;
                     case 2:
                         radioButton = (RadioButton) findViewById(R.id.feeling_groomy);
-                        sentences = getResources().getStringArray(R.array.feeling_groomy);
+                        radioButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                for(RadioButton bt : mRadioButtons) {
+                                    bt.setChecked(false);
+                                }
+                                feeling = Feeling.GROOMY;
+                                ((RadioButton)view).setChecked(true);
+                                button.setVisibility(View.VISIBLE);
+                            }
+                        });
                         break;
                     case 3:
                         radioButton = (RadioButton) findViewById(R.id.feeling_stifled);
-                        sentences = getResources().getStringArray(R.array.feeling_stifled);
+                        radioButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                for(RadioButton bt : mRadioButtons) {
+                                    bt.setChecked(false);
+                                }
+                                feeling = Feeling.STIFLED;
+                                ((RadioButton)view).setChecked(true);
+                                button.setVisibility(View.VISIBLE);
+                            }
+                        });
                         break;
                 }
 
                 if(radioButton != null) {
-                    index = r.nextInt(sentences.length);
-                    radioButton.setText(sentences[index]);
-                    //폰트 설정
                     radioButton.setTypeface(CustomFont.getInstance().getTypeface());
-
+                    mRadioButtons.add(radioButton);
                 }
             }
         }
@@ -83,20 +113,6 @@ public class FeelingActivity extends AppCompatActivity {
         button.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view) {
-                switch (radioGroup.getCheckedRadioButtonId()) {
-                    case R.id.feeling_groomy:
-                        feeling = Feeling.GROOMY;
-                        break;
-                    case R.id.feeling_happy:
-                        feeling = Feeling.HAPPY;
-                        break;
-                    case R.id.feeling_miss:
-                        feeling = Feeling.MISS;
-                        break;
-                    case R.id.feeling_stifled:
-                        feeling = Feeling.STIFLED;
-                        break;
-                }
 
                 //Selected feelings post to server
 
